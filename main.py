80c0aba4d1a5ae49a152521db6bba77682044886
ydoo123 <ydoo123@gmail.com>
2023-05-07 21:01:00 +0900

    Fix: fix bug with np.array

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ from fn import draw_single
 
 from Track.Tracker import Detection, Tracker
 from ActionsEstLoader import TSSTG
+import json
 
 
 ACTION_DICT = {"pending..": 0, "Fall Down": 1, "Lying Down": 2}
+# get url from url.json
+with open("url.json") as f:
+    URL = json.load(f)
+    URL = URL["url"]
 
 source = 2  # 2 for usb_cam
 
@@ -25,8 +30,10 @@ ACTION_CHECK_RATE = 0.1  # to check action every 0.1 second
 ACTION_COUNT_VALUE = 5
 
 
-def send_coord():
-    print("send_coord")
+def send_coord(bbox):
+    # calculate center of bbox
+    coord = (bbox[0] + bbox[2]) / 2
+    print(coord)
     return None
 
 
@@ -103,6 +110,7 @@ if __name__ == "__main__":
     # for check action
     prev_time = time.time()
     action_history = np.zeros(20)
+    action_name = "pending.."
 
     # DETECTION MODEL.
     inp_dets = args.detection_input_size
@@ -268,13 +276,13 @@ if __name__ == "__main__":
             Check action every 0.1 second.
             Send coordinate to server if action is "Fall Down"
             """
-            action_history.append(ACTION_DICT[action_name])
+            np.append(action_history, ACTION_DICT[action_name])
             action_history = action_history[1:]
 
-            if action_history.count(0) < ACTION_COUNT_VALUE:
+            if np.count_nonzero(action_history == 0) < ACTION_COUNT_VALUE:
                 # If there are fewer 0 (default) in the array, it means it has fallen
                 # It would be easier to count 0 than to count 1 and 2.
-                send_coord()
+                send_coord(bbox)
 
             prev_time = time.time()
 
